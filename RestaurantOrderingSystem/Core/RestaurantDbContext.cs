5a9ffc41cb4a08525f65f81b87bb06a4a2a19071
Dmitriy Ponomarev <pnomarevd38@mail.ru>
2023-04-20 15:58:31 +0300

    db begin

---
+++ RestaurantOrderingSystem/Core/RestaurantDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantOrderingSystem.Models.DbTables;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RestaurantOrderingSystem.Core
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+
+        /*public RestaurantDbContext() : base("Data Source=myServer;Initial Catalog=myDatabase;User ID=myUser;Password=myPassword")
+        {
+        }*/
+    }
+}
---
+++ RestaurantOrderingSystem/Models/DbTables/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderingSystem.Models.DbTables
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string FoodStar { get; set; }
+        public string FoodVote { get; set; }
+        public int FoodPrice { get; set; }
+        public int FoodDiscount { get; set; }
+        public string FoodDescription { get; set; }
+        public string FoodStatus { get; set; }
+        public string FoodType { get; set;}
+        public string FoodCategory { get; set; }
+        public byte[]? FoodImage { get; set; }
+
+    }
+}
