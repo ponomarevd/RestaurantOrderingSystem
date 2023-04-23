4149d3769721d6a31ed7d090e79a026e15b87593
user <user@WIN-DGO65FJKPJ7>
2023-04-23 16:41:38 +0300

    dom

--- RestaurantOrderingSystem/Core/RestaurantDbContext.cs
+++ RestaurantOrderingSystem/Core/RestaurantDbContext.cs
@@ -15,8 +15,8 @@ namespace RestaurantOrderingSystem.Core
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //  DESKTOP-467GVQM   WIN-5RHJKOR4UQF
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-467GVQM\SQLEXPRESS;Initial Catalog=restaurant_ordering_system_db;Integrated security=True;TrustServerCertificate=True");
+            //  квартира: DESKTOP-467GVQM   ноут: WIN-5RHJKOR4UQF   дом: WIN-DGO65FJKPJ7
+            optionsBuilder.UseSqlServer(@"Data Source=WIN-DGO65FJKPJ7\SQLEXPRESS;Initial Catalog=restaurant_ordering_system_db;Integrated security=True;TrustServerCertificate=True");
         }
         public DbSet<Food> Food { get; set; }
     }
