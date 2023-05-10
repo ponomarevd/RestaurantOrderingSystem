11eb07f1677fa48220ddb548d5bd713a7b2610f1
Dmitriy Ponomarev <pnomarevd38@mail.ru>
2023-05-10 11:44:05 +0300

    cart develop

--- RestaurantOrderingSystem/ViewModels/CartPageViewModel.cs
+++ RestaurantOrderingSystem/ViewModels/CartPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.EntityFrameworkCore;
 using RestaurantOrderingSystem.Core;
 using RestaurantOrderingSystem.Models.DbTables;
@@ -19,6 +20,15 @@ namespace RestaurantOrderingSystem.ViewModels
         private RestaurantDbContext _dbContext;
 
         [ObservableProperty]
+        private int _productsCount;
+
+        [ObservableProperty]
+        private int _productsSummary;
+
+        [ObservableProperty]
+        private string _countText;
+
+        [ObservableProperty]
         private ObservableCollection<FoodContain> _cartItems;
 
         [ObservableProperty]
@@ -37,14 +47,23 @@ namespace RestaurantOrderingSystem.ViewModels
         {
             try
             {
+                ProductsCount = 0;
+                ProductsSummary = 0;
+
                 ProgressRingVisibility = Visibility.Visible;
 
                 _dbContext = await Task.Run(() => new RestaurantDbContext());
                 _mainWindowViewModel = App.GetService<MainWindowViewModel>();
                 
                 _mainWindowViewModel.IsCartFilled = true;
+                ProductsCount = _mainWindowViewModel.BadgeValue;
 
                 CartItems = await Task.Run(() => new ObservableCollection<FoodContain>(_dbContext.FoodContain.Include(x => x.Food).Include(x => x.Cart).Where(x => x.Cart.UserID == _mainWindowViewModel.UserID)));
+
+                foreach(FoodContain item in CartItems)
+                    ProductsSummary += item.Count * item.Food.FoodPrice;
+
+                GetNumEnding(ProductsCount);
 
                 ProgressRingVisibility = Visibility.Hidden;
             }
@@ -52,8 +71,44 @@ namespace RestaurantOrderingSystem.ViewModels
             {
                 MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
+            }          
+        }
+
+        [RelayCommand]
+        private void DecrementCount()
+        {
+
+        }
+
+        [RelayCommand]
+        private void IncrementCount()
+        {
+
+        }
+
+        public void GetNumEnding(int iNumber)
+        {
+            iNumber = iNumber % 100;
+            if (iNumber >= 11 && iNumber <= 19)
+                CountText = "товаров";
+            else
+            {
+                var i = iNumber % 10;
+                switch (i)
+                {
+                    case (1):
+                        CountText = "товар";
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        CountText = "товара";
+                        break;
+                    default:
+                        CountText = "товаров";
+                        break;
+                }
             }
-            
         }
     }
 }
