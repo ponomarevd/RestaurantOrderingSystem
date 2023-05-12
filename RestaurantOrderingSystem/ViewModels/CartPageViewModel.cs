13968c6892041102f044ce0679c48138d14fd7d4
Dmitriy Ponomarev <pnomarevd38@mail.ru>
2023-05-12 10:03:23 +0300

    cart fix

--- RestaurantOrderingSystem/ViewModels/CartPageViewModel.cs
+++ RestaurantOrderingSystem/ViewModels/CartPageViewModel.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
 
 namespace RestaurantOrderingSystem.ViewModels
 {
@@ -18,6 +19,21 @@ namespace RestaurantOrderingSystem.ViewModels
     {
         private MainWindowViewModel? _mainWindowViewModel;
         private RestaurantDbContext _dbContext;
+        private INavigationService? navService;
+
+        [ObservableProperty]
+        private ObservableCollection<FoodContain> _cartItems;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(InterfaceIsEnabled))]
+        private Visibility _progressRingVisibility = Visibility.Hidden;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(EmptyCartVisibility))]
+        private Visibility _interfaceVisibility = Visibility.Visible;
+
+        [ObservableProperty]
+        private string _countText;
 
         [ObservableProperty]
         private int _productsCount;
@@ -26,16 +42,13 @@ namespace RestaurantOrderingSystem.ViewModels
         private int _productsSummary;
 
         [ObservableProperty]
-        private string _countText;
+        private bool _buttonsCountIsEnabled = true;
 
         [ObservableProperty]
-        private ObservableCollection<FoodContain> _cartItems;
+        private bool _deleteBtnIsEnabled = true;
+        public bool InterfaceIsEnabled => ProgressRingVisibility == Visibility.Hidden ? true : false;
+        public Visibility EmptyCartVisibility => InterfaceVisibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
 
-        [ObservableProperty]
-        private Visibility _progressRingVisibility = Visibility.Hidden;
-
-        [ObservableProperty]
-        private bool _interfaceIsEnabled = true;
         public void OnNavigatedFrom()
         {
             _mainWindowViewModel.IsCartFilled = false;
@@ -44,33 +57,39 @@ namespace RestaurantOrderingSystem.ViewModels
         public void OnNavigatedTo()
         {
             InitializeViewModel();
+            _mainWindowViewModel.IsCartFilled = true;
         }
 
         private async void InitializeViewModel()
         {
             try
             {
-                ProductsCount = 0;
-                ProductsSummary = 0;
-
-                InterfaceIsEnabled = false;
-                ProgressRingVisibility = Visibility.Visible;
-
-                _dbContext = await Task.Run(() => new RestaurantDbContext());
                 _mainWindowViewModel = App.GetService<MainWindowViewModel>();
-                
-                _mainWindowViewModel.IsCartFilled = true;
-                ProductsCount = _mainWindowViewModel.BadgeValue;
 
-                CartItems = await Task.Run(() => new ObservableCollection<FoodContain>(_dbContext.FoodContain.Include(x => x.Food).Include(x => x.Cart).Where(x => x.Cart.UserID == _mainWindowViewModel.UserID)));
+                if(_mainWindowViewModel.BadgeValue == 0)
+                    InterfaceVisibility = Visibility.Hidden;
+                else
+                {
+                    InterfaceVisibility = Visibility.Visible;
+                    ProductsCount = 0;
+                    ProductsSummary = 0;
 
-                foreach(FoodContain item in CartItems)
-                    ProductsSummary += item.Count * item.Food.FoodPrice;
+                    ProgressRingVisibility = Visibility.Visible;
 
-                GetNumEnding(ProductsCount);
+                    _dbContext = await Task.Run(() => new RestaurantDbContext());
 
-                InterfaceIsEnabled = true;
-                ProgressRingVisibility = Visibility.Hidden;
+                    _mainWindowViewModel.IsCartFilled = true;
+                    ProductsCount = _mainWindowViewModel.BadgeValue;
+
+                    CartItems = await Task.Run(() => new ObservableCollection<FoodContain>(_dbContext.FoodContain.Include(x => x.Food).Include(x => x.Cart).Where(x => x.Cart.UserID == _mainWindowViewModel.UserID)));
+
+                    foreach (FoodContain item in CartItems)
+                        ProductsSummary += item.Count * item.Food.FoodPrice;
+
+                    GetNumEnding(ProductsCount);
+
+                    ProgressRingVisibility = Visibility.Hidden;
+                }
             }
             catch (Exception ex)
             {
@@ -80,29 +99,76 @@ namespace RestaurantOrderingSystem.ViewModels
         }
 
         [RelayCommand]
+        private void GoToMenu()
+        {
+            navService = App.GetService<INavigationService>();
+            navService.Navigate(typeof(Views.Pages.MenuPage));
+        }
+
+        [RelayCommand]
         private async void DecrementCount(int foodContainId)
         {
+            ButtonsCountIsEnabled = false;
             FoodContain foodContainModel = await Task.Run(() => _dbContext.FoodContain.FirstOrDefault(x => x.FoodContainID == foodContainId));
-            foodContainModel.Count--;
 
-            _mainWindowViewModel.BadgeValue--;
-            ProductsCount--;
+            if (foodContainModel.Count == 1)
+            {
+                ButtonsCountIsEnabled = true;
+                return;
+            }
+            else
+            {
+                foodContainModel.Count--;
 
+                _mainWindowViewModel.BadgeValue--;
+                ProductsCount--;
+
+                await _dbContext.SaveChangesAsync();
+                InitializeViewModel();
+            }
+            ButtonsCountIsEnabled = true;
+        }
+
+        [RelayCommand]
+        private async void DeleteItem(int foodContainId)
+        {
+            DeleteBtnIsEnabled = false;
+
+            FoodContain foodContainModel = await Task.Run(() => _dbContext.FoodContain.FirstOrDefault(x => x.FoodContainID == foodContainId));
+
+            _mainWindowViewModel.BadgeValue -= foodContainModel.Count;
+            ProductsCount -= foodContainModel.Count;
+
+            _dbContext.FoodContain.Remove(foodContainModel);
             await _dbContext.SaveChangesAsync();
+
             InitializeViewModel();
+
+            DeleteBtnIsEnabled = true;
         }
 
         [RelayCommand]
         private async void IncrementCount(int foodContainId)
         {
+            ButtonsCountIsEnabled = false;
             FoodContain foodContainModel = await Task.Run(() => _dbContext.FoodContain.FirstOrDefault(x => x.FoodContainID == foodContainId));
-            foodContainModel.Count++;
 
-            _mainWindowViewModel.BadgeValue++;
-            ProductsCount++;
+            if (foodContainModel.Count == 5)
+            {
+                ButtonsCountIsEnabled = true;
+                return;
+            }
+            else
+            {
+                foodContainModel.Count++;
 
-            await _dbContext.SaveChangesAsync();
-            InitializeViewModel();
+                _mainWindowViewModel.BadgeValue++;
+                ProductsCount++;
+
+                await _dbContext.SaveChangesAsync();
+                InitializeViewModel();
+            }
+            ButtonsCountIsEnabled = true;
         }
 
         public void GetNumEnding(int iNumber)
