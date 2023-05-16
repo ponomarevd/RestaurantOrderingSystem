1dd5a271e6546005da60ba6c769d1d1d3670ff6c
Dmitriy Ponomarev <pnomarevd38@mail.ru>
2023-05-16 21:05:09 +0300

    fixes

--- RestaurantOrderingSystem/Core/RestaurantDbContext.cs
+++ RestaurantOrderingSystem/Core/RestaurantDbContext.cs
@@ -16,7 +16,7 @@ namespace RestaurantOrderingSystem.Core
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //  квартира: DESKTOP-467GVQM   ноут: HOME-PC   дом: WIN-DGO65FJKPJ7
-            optionsBuilder.UseSqlServer(@"Data Source=HOME-PC\SQLEXPRESS;Initial Catalog=restaurant_ordering_system_db;Integrated security=True;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-467GVQM\SQLEXPRESS;Initial Catalog=restaurant_ordering_system_db;Integrated security=True;TrustServerCertificate=True");
         }
         public DbSet<Food> Food { get; set; }
         public DbSet<FoodContain> FoodContain { get; set; }
